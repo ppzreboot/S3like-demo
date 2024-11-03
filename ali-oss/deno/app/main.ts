d632ea6a9e1b80dd39ceec8fb2c58cdf963e22e2
PPz <ppzreboot@qq.com>
2024-11-03 15:25:08 +0800

    feat(ali-oss in Deno): client

--- ali-oss/deno/app/main.ts
+++ ali-oss/deno/app/main.ts
@@ -1,3 +1,18 @@
 Deno.serve(async req => {
-  return new Response('hello')
+  const url = new URL(req.url)
+  switch (url.pathname) {
+    case '/':
+      return await serve_static()
+    case '/api/auth':
+      return new Response('hello')
+    default:
+      return new Response('Not Found', { status: 404 })
+  }
 })
+
+async function serve_static() {
+  const webapp = await Deno.open('./app/client/index.html', {
+    read: true,
+  })
+  return new Response(webapp.readable)
+}
